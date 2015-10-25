d91804519e525876502ad1580ad3fbbc83f6a5c4
Kyle Smith <kmaismith@gmail.com>
2015-10-25 01:27:49 -0400

    added a shell script to quickly upgrade or update an already running starmade instance

---
+++ upgrade.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "you must specify the name of a running starmade docker instance"
+    exit 1
+fi 
+
+INSTANCE_NAME=$1
+
+NEW_INSTANCE_ID=$(docker create --volumes-from=$INSTANCE_NAME -t starmade)
+docker stop $INSTANCE_NAME && docker rm $INSTANCE_NAME
+
+docker rename $NEW_INSTANCE_ID $INSTANCE_NAME
+docker start $INSTANCE_NAME
