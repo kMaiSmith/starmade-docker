6858b4cd990812d06858690037805000d0189736
Kyle Smith <kmaismith@gmail.com>
2015-11-01 01:43:24 -0400

    refactored backup.sh to not leave backup tarballs in the starmade docker container

--- backup.sh
+++ backup.sh
@@ -7,10 +7,10 @@ fi
 
 mkdir -p backups
 
-INSTANCE_NAME=$!
+INSTANCE_NAME=$1
 DATE_TAG=`date +"%Y%m%d-%H%M"`
 
-docker exec starmade_1 tar -czvf /opt/starmade-backup-${DATE_TAG}.tar.gz -C /opt StarMade
-docker cp starmade_1:/opt/starmade-backup-${DATE_TAG}.tar.gz backups/
+docker run --name starmade_backup --volumes-from $INSTANCE_NAME -v $(pwd)/backups:/backup ubuntu tar -czvf /backup/starmade-backup-${DATE_TAG}.tar.gz -C /opt StarMade
+docker rm starmade_backup
 
 echo "Backup created at backups/starmade-backup-${DATE_TAG}.tar.gz"
